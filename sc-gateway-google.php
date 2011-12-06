da8cf0c9ed550c028e98e58230817eb0f419dccd
Naatan <n.rijksen@gmail.com>
2011-12-06 14:03:45 -0500

    Initial commit, based on original code from the Social Connect plugin

---
+++ sc-gateway-google.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+Plugin Name: Social Connect - Google Gateway
+Plugin URI: http://wordpress.org/extend/plugins/social-connect/
+Description: Allows you to login / register with Google - REQUIRES Social Connect plugin
+Version: 0.10
+Author: Brent Shepherd, Nathan Rijksen
+Author URI: http://wordpress.org/extend/plugins/social-connect/
+License: GPL2
+ */
+
+require_once dirname(__FILE__) . '/openid.php';
+
+class SC_Gateway_Google 
+{
+	
+	protected static $calls = array('connect','callback');
+	
+	static function init()
+	{
+		add_action('social_connect_button_list',array('SC_Gateway_Google','render_button'));
+	}
+	
+	static function call()
+	{
+		if ( !isset($_GET['call']) OR !in_array($_GET['call'], array('connect','callback')))
+		{
+			return;
+		}
+		
+		call_user_func(array('SC_Gateway_Google', $_GET['call']));
+	}
+	
+	static function render_button()
+	{
+		$image_url = plugins_url() . '/' . basename( dirname( __FILE__ )) . '/button.png';
+		?>
+		<a href="javascript:void(0);" title="Google" class="social_connect_login_google"><img alt="Google" src="<?php echo $image_url ?>" /></a>
+		<div id="social_connect_google_auth" style="display: none;">
+			<input type="hidden" name="redirect_uri" value="<?php echo( SOCIAL_CONNECT_PLUGIN_URL . '/call.php?call=connect&gateway=google' ); ?>" />
+		</div>
+		
+		<script type="text/javascript">
+		(jQuery(function($) {
+			var _do_google_connect = function() {
+				var google_auth = $('#social_connect_google_auth');
+				var redirect_uri = google_auth.find('input[type=hidden][name=redirect_uri]').val();
+				window.open(redirect_uri,'','scrollbars=no,menubar=no,height=400,width=800,resizable=yes,toolbar=no,status=no');
+			};
+			
+			$(".social_connect_login_google, .social_connect_login_continue_google").click(function() {
+				_do_google_connect();
+			});
+		}));
+		</script>
+		
+		<?php
+	}
+	
+	static function connect()
+	{
+		$openid = new LightOpenID;
+		$openid->identity = 'https://www.google.com/accounts/o8/id';
+		$openid->required = array('namePerson/first', 'namePerson/last', 'contact/email');
+		$openid->returnUrl = SOCIAL_CONNECT_PLUGIN_URL . '/call.php?gateway=google&call=callback';
+		header('Location: ' . $openid->authUrl());
+	}
+	
+	static function callback()
+	{
+		$openid 			= new LightOpenID;
+		$openid->returnUrl 	= SOCIAL_CONNECT_PLUGIN_URL . '/call.php?gateway=google&call=callback';
+		
+		try
+		{
+			if ( !$openid->validate())
+			{
+				echo 'validation failed';
+				return;
+			}
+		}
+			catch(ErrorException $e)
+		{
+			echo $e->getMessage();
+			return;
+		}
+		
+		$google_id 	= $openid->identity;
+		$attributes = $openid->getAttributes();
+		$email 		= $attributes['contact/email'];
+		$first_name 	= $attributes['namePerson/first'];
+		$last_name 	= $attributes['namePerson/last'];
+		$signature 	= SC_Utils::generate_signature($google_id);
+		
+		?>
+		<html>
+		<head>
+		<script>
+		function init() {
+		  window.opener.wp_social_connect({'action' : 'social_connect', 'social_connect_provider' : 'google', 
+			'social_connect_openid_identity' : '<?php echo $google_id ?>',
+			'social_connect_signature' : '<?php echo $signature ?>',
+			'social_connect_email' : '<?php echo $email ?>',
+			'social_connect_first_name' : '<?php echo $first_name ?>',
+			'social_connect_last_name' : '<?php echo $last_name ?>'});
+			
+		  window.close();
+		}
+		</script>
+		</head>
+		<body onload="init();">
+		</body>
+		</html>
+		<?php
+	}
+	
+	static function process_login()
+	{
+		$redirect_to 			= SC_Utils::redirect_to();
+		$provider_identity 		= $_REQUEST[ 'social_connect_openid_identity' ];
+		$provided_signature 	= $_REQUEST[ 'social_connect_signature' ];
+		
+		SC_Utils::verify_signature( $provider_identity, $provided_signature, $redirect_to );
+		
+		return (object) array(
+			'provider_identity' => $provider_identity,
+			'email' 			=> $_REQUEST[ 'social_connect_email' ],
+			'first_name' 		=> $_REQUEST[ 'social_connect_first_name' ],
+			'last_name' 		=> $_REQUEST[ 'social_connect_last_name' ],
+			'profile_url'		=> '',
+			'name' 				=> $_REQUEST[ 'social_connect_first_name' ] . ' ' . $_REQUEST[ 'social_connect_last_name' ],
+			'user_login' 		=> strtolower($_REQUEST[ 'social_connect_first_name' ] . $_REQUEST[ 'social_connect_last_name' ])
+		);
+	}
+	
+}
+
+SC_Gateway_Google::init();
